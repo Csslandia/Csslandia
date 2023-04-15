414a9546a02166ea921bf9b34762aa4858024101
jacobheap <jacobheap0@gmail.com>
2023-04-15 09:04:20 +0200

    bug fixes #2

--- js/dev.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import sass from "sass";
-
-const result = sass.compile("scss/csslandia.scss");
-const compressed = sass.compile("scss/csslandia.scss", { style: "compressed" });
-
-fs.writeFileSync("style.css", result.css);
-fs.writeFileSync("style.min.css", compressed.css);
